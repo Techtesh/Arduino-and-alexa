efd52090d7f129ddb9149700c6b92b27d42cd445
Techtesh <hitesh.jetwani@gmail.com>
2020-12-06 09:05:15 +0530

    Add files via upload

---
+++ alexa_and_arduino.ino
@@ -0,0 +1,67 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include "fauxmoESP.h"
+
+#define ssid "admin"
+#define pass "12345678"
+
+#define device1 "bedroom light"
+#define device2 "bedroom fan"
+#define device3 "smart socket"
+
+fauxmoESP fauxmo; 
+
+void wifi() 
+{
+  WiFi.mode(WIFI_STA);
+
+  WiFi.begin(ssid,pass);
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(100);
+  }
+}
+
+void setup() 
+{
+  Serial.begin(115200);
+  wifi();
+  fauxmo.createServer(true);
+  fauxmo.setPort(80);
+
+  fauxmo.enable(true);
+  fauxmo.addDevice(device1);
+  fauxmo.addDevice(device2);
+  fauxmo.addDevice(device3);
+
+  fauxmo.onSetState([](unsigned char device_id, const char * device_name, bool state, unsigned char value) {
+  Serial.printf("[MAIN](%s) state: %s\n",device_name, state ? "ON" : "OFF");
+    if (strcmp(device_name, device1) == 0)
+    {
+      if (state)
+        Serial.print("1");
+      else
+        Serial.print("2");
+    }
+    if (strcmp(device_name, device2) == 0)
+    {
+      if (state)
+        Serial.print("3");
+      else
+        Serial.print("4");
+    }
+
+    if (strcmp(device_name, device3) == 0)
+    {
+      if (state)
+        Serial.print("5");
+      else
+        Serial.print("6");
+    }
+  });
+}
+
+void loop() 
+{
+  fauxmo.handle();
+}
